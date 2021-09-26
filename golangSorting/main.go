73b30f4a931966cb557aa2a41481485a6ebe7dbb
johnkennedyls <jkls1998@gmail.com>
2021-09-25 22:47:33 -0500

    Analisis complejidad temporal, and bubleSort

--- golangSorting/main.go
+++ golangSorting/main.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var s string = "Hola Mundo"
-	var b string = "Chao Mundo"
 
-	fmt.Println(s, b)
 	fmt.Println(insertionSort([]int{3, 5, 6, 3, 7, 8, 2, 4}))
+	fmt.Println(bubbleSort([]int{3, 5, 6, 3, 7, 8, 2, 4}))
 }
 
+//based on: https://www.youtube.com/watch?v=vg4GS4LSJXI
 func insertionSort(A []int) []int {
 	for j := 0; j < len(A); j++ {
 		key := A[j]
@@ -23,6 +22,23 @@ func insertionSort(A []int) []int {
 	return A
 }
 
-func selectionSort(A []int) []int {
+//Based on: https://tutorialedge.net/courses/go-algorithms-course/21-bubble-sort-in-go/
+func bubbleSort(input []int) []int {
+	n := len(input)
+	swapped := true
+	for swapped {
 
+		swapped = false
+
+		for i := 0; i < n-1; i++ {
+
+			if input[i] > input[i+1] {
+
+				input[i], input[i+1] = input[i+1], input[i]
+
+				swapped = true
+			}
+		}
+	}
+	return input
 }
